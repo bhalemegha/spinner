0772ce5d2b0ed7bebbe6fb240f20fe7d7aad6f50
bhalemegha <meghabhale6@gmail.com>
2021-11-02 16:30:23 +0000

    Refactor spinner

--- spinner2.js
+++ spinner2.js
@@ -1,37 +1,6 @@
-setTimeout(() => {
-  process.stdout.write('\r|   ');
-}, 100);
-
-setTimeout(() => {
-  process.stdout.write('\r/   ');
-}, 300);
-
-setTimeout(() => {
-  process.stdout.write('\r-   ');
-}, 500);
-
-setTimeout(() => {
-  process.stdout.write('\r\\   '); 
-}, 700);
-
-setTimeout(() => {
-  process.stdout.write('\r|   ');
-}, 900);
-
-
-setTimeout(() => {
-  process.stdout.write('\r//   ');
-}, 1200);
-
-setTimeout(() => {
-  process.stdout.write('\r-   ');
-}, 1200);
-
-setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
-  process.stdout.write('\r\\   '); 
-}, 1500);
-
-setTimeout(() => {
-  process.stdout.write('\r|   ');
-}, 1800);
+const spinner = function(){
+  let arr = ['|','/','-','\\'];
+  for (let i = 0, timeOut = 100;timeOut <= 1800; i++,timeOut += 200) {
+    setTimeout(() => process.stdout.write(`\r${arr[i]}   `), timeOut);
+  }
+};
